6daff19116f32913bc562d715ee5f87b546ae9cb
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-03 23:45:27 +0900

    Feature : JdbcMemberRepository 추가 / 순수JDBC * SpringConfig에서 JdbcMemberRepository DI 연결

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,19 +1,35 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-  @Bean
-  MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+  // memberRepository() 메서드를 호출해서 MemoryMemberRepository를 스프링 빈에 등록한다.
+  // @Bean
+  // MemberRepository memberRepository() {
+  //   return new MemoryMemberRepository();
+  // }
+
+
+  // memberRepository() 메서드를 호출해서 JdbcMemberRepository를 스프링 빈에 등록한다.
+  private final DataSource dataSource;
+
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
+  @Bean
+  MemberRepository memberRepository() {
+    return new JdbcMemberRepository(dataSource);
+  }
+
+  // memberService() 메서드를 호출해서 MemberService를 스프링 빈에 등록한다.
   @Bean
   MemberService memberService() {
     return new MemberService(memberRepository());
