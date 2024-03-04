2cc9061aef42a723de1af908c9c11d212565fe01
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-04 19:54:32 +0900

    Feature : JpaTemplateMemberRepository 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,10 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,11 +21,11 @@ public class SpringConfig {
 
 
     // memberRepository() 메서드를 호출해서 JdbcMemberRepository를 스프링 빈에 등록한다.
-    private final DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+//    private final DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     // JdbcMemberRepository는 DataSource를 주입받아야 하므로 생성자를 통해 주입받는다.
 //  @Bean
@@ -31,12 +33,19 @@ public class SpringConfig {
 //    return new JdbcMemberRepository(dataSource);
 //  }
 
+    private final EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
     // JdbcTemplateMemberRepository는 DataSource를 주입받아야 하므로 생성자를 통해 주입받는다.
     @Bean
     MemberRepository memberRepository() {
 //         return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
     // memberService() 메서드를 호출해서 MemberService를 스프링 빈에 등록한다.
