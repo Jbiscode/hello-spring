f8e90fc35d2ceb68beee1d1731172932195489e4
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-04 19:30:23 +0900

    Feature : JdbcTemplateMemberRepository 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,37 +1,47 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-  // memberRepository() 메서드를 호출해서 MemoryMemberRepository를 스프링 빈에 등록한다.
-  // @Bean
-  // MemberRepository memberRepository() {
-  //   return new MemoryMemberRepository();
-  // }
+    // memberRepository() 메서드를 호출해서 MemoryMemberRepository를 스프링 빈에 등록한다.
+    // @Bean
+    // MemberRepository memberRepository() {
+    //   return new MemoryMemberRepository();
+    // }
 
 
-  // memberRepository() 메서드를 호출해서 JdbcMemberRepository를 스프링 빈에 등록한다.
-  private final DataSource dataSource;
+    // memberRepository() 메서드를 호출해서 JdbcMemberRepository를 스프링 빈에 등록한다.
+    private final DataSource dataSource;
 
-  public SpringConfig(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-  @Bean
-  MemberRepository memberRepository() {
-    return new JdbcMemberRepository(dataSource);
-  }
+    // JdbcMemberRepository는 DataSource를 주입받아야 하므로 생성자를 통해 주입받는다.
+//  @Bean
+//  MemberRepository memberRepository() {
+//    return new JdbcMemberRepository(dataSource);
+//  }
 
-  // memberService() 메서드를 호출해서 MemberService를 스프링 빈에 등록한다.
-  @Bean
-  MemberService memberService() {
-    return new MemberService(memberRepository());
-  }
+    // JdbcTemplateMemberRepository는 DataSource를 주입받아야 하므로 생성자를 통해 주입받는다.
+    @Bean
+    MemberRepository memberRepository() {
+//         return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+
+    // memberService() 메서드를 호출해서 MemberService를 스프링 빈에 등록한다.
+    @Bean
+    MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
 }
