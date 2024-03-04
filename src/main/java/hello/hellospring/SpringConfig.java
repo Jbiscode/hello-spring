4610d82bbdda6bb727477935effb887a4c009352
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-04 20:24:07 +0900

    Feature : SpringDataJpaMemberRepository 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,7 +12,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     // memberRepository() 메서드를 호출해서 MemoryMemberRepository를 스프링 빈에 등록한다.
     // @Bean
     // MemberRepository memberRepository() {
@@ -33,24 +37,25 @@ public class SpringConfig {
 //    return new JdbcMemberRepository(dataSource);
 //  }
 
-    private final EntityManager em;
+//    private final EntityManager em;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     // JdbcTemplateMemberRepository는 DataSource를 주입받아야 하므로 생성자를 통해 주입받는다.
-    @Bean
-    MemberRepository memberRepository() {
+//    @Bean
+//    MemberRepository memberRepository() {
 //         return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
+
 
     // memberService() 메서드를 호출해서 MemberService를 스프링 빈에 등록한다.
     @Bean
     MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 }
